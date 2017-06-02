9bf4260c3700c1a0dbe08796c0f65b28e1d4b51f
SAPIENT\smuru3 <smurugan@sapient.com>
2017-06-02 22:45:15 +0530

    Add existing file

--- falcor/Readme.txt
+++ falcor/Readme.txt
@@ -1,6 +1,4 @@
 1.Trying to read the  static json stored 
-steps to reproduce-1. node index1.js
-                   2.on browser willbe able  to see the view
-
+steps to reproduce-TO see how on changing the rating both place is changed. 
 
                    
---
+++ falcor/rating.js
@@ -0,0 +1,40 @@
+var model = new falcor.Model({
+  cache: {
+    titlesById: {
+      5212: {
+        name: "House of Cards",
+        rating: 4.5
+      }
+    },
+    genreList: [
+      {
+        name: "Recently Watched",
+        titles: [
+          { $type: "ref", value: ["titlesById", 5212] }
+        ]
+      },
+      {
+        name: "New Releases",
+        titles: [
+          { $type: "ref", value: ["titlesById", 5212] }
+        ]
+      }
+    ]
+  }
+});
+var myrate
+$(".rate").keyup(function(){
+            alert($(this).val());
+            myrate=$(this).val();
+
+          });
+model
+  .setValue('genreList[0].titles[0].rating',4)
+  .then(function(value){
+    model
+      .get('genreList[0..1].titles[0]["name", "rating"]')
+      .then(function(json){
+        console.log(JSON.stringify(json, null, 4));
+        document.getElementById('output').innerHTML = JSON.stringify(json, null, 4);
+      });
+  });
