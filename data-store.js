838aa45e0705462f73817241041b3d94309697d7
SAPIENT\smuru3 <smurugan@sapient.com>
2017-06-08 19:01:58 +0530

    no message

--- data-store.js
+++ data-store.js
@@ -3,16 +3,16 @@ $(window).load(function() {
 var model = new falcor.Model({
   cache: {
     titlesById: {
-      5212:{ title: "cool start", price: 40.00, picture: "img/image3.jpg" ,skuid:"113",rate:"3"},
+      5212:{ title: "machop", price: 40.00, picture: "img/image3.jpg" ,skuid:"113",rate:"3",description:"MACHOP’s muscles are special - they never get sore no matter how much they are used in exercise. This POKéMON has sufficient power to hurl a hundred adult humans."},
     },
     productList: [
       {
         name: "All products",
         products: [
             { $type: "ref", value: ["titlesById", 5212] },
-            { title: " pikachu", price: 37.79, picture: "img/image1.jpg" ,skuid:"111",rate:"2"},
-            { title: " watch Unleashed", price: 44.99, picture: "img/image2.jpg" ,skuid:"112",rate:"4" },
-            { title: " Unleashed iPhone", price: 44.99, picture: "img/image4.jpg" ,skuid:"114",rate:"1" },
+            { title: " pikachu", price: 37.79, picture: "img/image1.jpg" ,skuid:"111",rate:"2",description:"Whenever PIKACHU comes across something new, it blasts it with a jolt of electricity. If you come across a blackened berry, it’s evidence that this POKéMON mistook the intensity of its charge." },
+            { title: " Haunter", price: 44.99, picture: "img/image2.jpg" ,skuid:"112",rate:"4",description:"HAUNTER is a dangerous POKéMON. If one beckons you while floating in darkness, you must never approach it. This POKéMON will try to lick you with its tongue and steal your life away." },
+            { title: " Nidoran", price: 44.99, picture: "img/image4.jpg" ,skuid:"114",rate:"1" ,description:"The male NIDORAN has developed muscles for moving its ears. Thanks to them, the ears can be freely moved in any direction. Even the slightest sound does not escape this POKéMON’s notice."},
         ]
       },
       {
@@ -28,7 +28,7 @@ var allProducts,offers,display,myprice,rateid;
   
  
       model
-      .get('productList[0..1].products[0..2]["title", "price","picture","skuid","rate"]')
+      .get('productList[0..1].products[0..2]["title", "price","picture","skuid","rate","description"]')
       .then(function(json){
         console.log(JSON.stringify(json, null, 4));
        
@@ -50,7 +50,7 @@ var allProducts,offers,display,myprice,rateid;
 		  .setValue('productList[0].products[0].rate',$('#'+rateid).val())
 		  .then(function(value){
 		  	model
-      .get('productList[0..1].products[0..2]["title", "price","picture","skuid","rate"]')
+      .get('productList[0..1].products[0..2]["skuid","rate"]')
       .then(function(json){
         console.log(JSON.stringify(json, null, 4));
          $('#'+skuid).find('.rate').html(json.json.productList[0].products[0].rate);
